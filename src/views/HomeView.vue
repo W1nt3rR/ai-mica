c82accddfede8508384ad942dcd6a6c291994507
W1nt3rR <frostywinterr@gmail.com>
2024-01-06 19:06:49 +0100

    Add initial project files and configurations.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="home"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+    #home {
+        width: 100%;
+        height: 100%;
+
+        background-color: rgb(69, 69, 69);
+
+        display: flex;
+    }
+</style>
