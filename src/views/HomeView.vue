3b83edda119122a71eb9a99df5c3c9793fe7f1b9
W1nt3rR <frostywinterr@gmail.com>
2024-01-07 23:52:08 +0100

    Initial testing of the game.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,35 @@
 <template>
-    <div id="home"></div>
+    <div id="home">
+        {{ gameState }}
+        <button @click="makeMove">Make move</button>
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import { ref, onMounted } from "vue";
+    import Client, { type ICalculateMoveDTO, type IMapObject } from "@/Client";
+
+    const gameState = ref<ICalculateMoveDTO>({
+        mapName: "map1",
+        difficulty: "easy",
+        gameState: {
+            playerTurn: "white",
+            pointsLeftoverBlack: 9,
+            pointsLeftoverWhite: 9,
+            pointsTaken: [],
+        },
+    });
+
+    const mapsData = ref<Array<IMapObject | null>>();
+
+    async function makeMove() {
+        gameState.value.gameState = await Client.calculateMove(gameState.value);
+    }
+
+    onMounted(async () => {
+        mapsData.value = await Client.maps();
+    });
+</script>
 
 <style scoped lang="scss">
     #home {
